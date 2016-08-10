2904e0e60f66645065fbe61fbd81e5686fc63014
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-10 00:14:38 -0500

    bool values

---
+++ app.js
@@ -0,0 +1,12 @@
+// This program displays values to represent true and false.
+var trueValue,
+    falseValue;
+
+var x = 5,
+    y = 10;
+
+trueValue = (x < y);
+falseValue = (y == x);
+
+console.log("True is " + trueValue);
+console.log("False is " + falseValue);
